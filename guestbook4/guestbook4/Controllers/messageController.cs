b04d31a4b7a9ef7942143f57e9f465d7d92e5057
githubike <732552273@qq.com>
2017-03-20 16:12:43 +0800

    留言板1.1

---
+++ guestbook4/guestbook4/Controllers/messageController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using guestbook4.Models;
+using MvcApplication1.Service;
+
+namespace guestbook4.Controllers
+{
+    public class messageController : Controller
+    {
+        //實例化Service
+        messageDBService data = new messageDBService();
+
+
+        // GET: /message/
+        public ActionResult Index()
+        {
+            //Article列表
+            return View(data.GetData());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            string id = Request.QueryString["id"];
+            int x;
+            if (!int.TryParse(id, out x))
+            {
+                //Response.Write("<script>alert('参数不完整');location.href='/'</script>");
+                //return null;
+            }
+            data.db.Table.Remove(data.db.Table.SingleOrDefault(a => a.Id == x));
+            data.db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Create(string strTitle, string strContent)
+        {
+            //調用Service中的Create方法,把數據寫到數據庫中
+            data.DBCreate(strTitle, strContent);
+            //重導向到 Action
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ guestbook4/guestbook4/Service/messageDBService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using guestbook4.Models;  //引用Model命名空間
+
+namespace MvcApplication1.Service
+{
+    public class messageDBService
+    {
+        //實例化實體數據
+        public GuestBookEntities db = new GuestBookEntities();
+        public List<Table> GetData()
+        {
+            return (db.Table.OrderByDescending(a =>a.Id).ToList());
+        }
+        //把從User接受的數據寫入messageEnitity
+        public void DBCreate(string strTitle, string strContent)
+        {
+            //實例化Artile對象
+            Table newData = new Table();
+
+            //給Artile對象的屬性賦值
+            newData.Title = strTitle;
+            newData.Content = strContent;
+            newData.Time = DateTime.Now;
+
+            //實體添加到Entity中
+            db.Table.Add(newData);
+            //保存到數據庫
+            db.SaveChanges();
+
+        }
+    }
+}
